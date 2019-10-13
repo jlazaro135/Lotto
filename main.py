5be62593af2c8f80e43b3aa0eb00d81730591399
Jesús Lázaro <jlazaro135@gmail.com>
2019-10-13 23:55:14 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+secret = 7
+
+
+guess = int(input ("guess the number :"))
+print (guess)
+
+
+if guess == secret:
+    print ("bieeeeeeeeen!")
+
+else:
+    print ("It's not " + str(guess) + " try again!")
